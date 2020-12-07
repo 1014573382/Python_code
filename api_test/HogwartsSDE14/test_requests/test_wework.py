86e8022c1754d39e0249e6df71aa9b3067ea901f
guonian <guoxiaoli1512@163.com>
2020-12-08 00:39:35 +0800

    接口测试——生成随机参数

--- api_test/HogwartsSDE14/test_requests/test_wework.py
+++ api_test/HogwartsSDE14/test_requests/test_wework.py
@@ -1,8 +1,27 @@
 #-*- coding:utf-8 -*-
 # @Author : guonian
 # @Time : 2020/12/02 23:18
+import re
+import random
+
 import pytest
 import requests
+
+
+def test_create_data():
+    "userid, name, mobile"
+    # 可考虑用生成器或迭代器，因为数据量大的时候列表会很占内存
+    # data = [(str(random.randint(0, 9999999)),
+    #          "薛之谦",
+    #          str(random.randint(13800000000, 13899900000))) for x in range(3)]
+
+    # 考虑到可能并发执行，会有重复的情况，对上面随机数进行改造
+    # %06d 加上x随机生成6位数，用0补够6位
+    data = [("202012xx" + str(x),
+             "薛之谦",
+             "13834%06d"%x) for x in range(20)]
+    # print(data)
+    return data
 
 
 class TestWework:
@@ -22,12 +41,12 @@ class TestWework:
         }
 
         r = requests.get("https://qyapi.weixin.qq.com/cgi-bin/gettoken",params=request_param)
-        print(r.json()['access_token'])
+        # print(r.json()['access_token'])
         return r.json()['access_token']
         # print(r)
 
     @pytest.mark.run(order=1)
-    def test_create(self,token, userid, name, alias, mobile, department=None):
+    def test_create(self,token, userid, mobile, name, alias, department=None):
         """
         创建成员
         请求方式：POST（HTTPS）
@@ -52,7 +71,7 @@ class TestWework:
         r = requests.post(
             f"https://qyapi.weixin.qq.com/cgi-bin/user/create?access_token={token}",
             json=request_body)
-        print(r.json())
+        # print(r.json())
         return r.json()
 
     @pytest.mark.second
@@ -64,7 +83,7 @@ class TestWework:
         :return:
         """
         r = requests.get(f"https://qyapi.weixin.qq.com/cgi-bin/user/get?access_token={token}&userid={userid}")
-        print(r.json())
+        # print(r.json())
         return r.json()
 
     @pytest.mark.run(order=3)
@@ -92,7 +111,7 @@ class TestWework:
         }
 
         r = requests.post(f"https://qyapi.weixin.qq.com/cgi-bin/user/update?access_token={token}",json=request_body)
-        print(r.json())
+        # print(r.json())
         return r.json()
 
     @pytest.mark.skip
@@ -106,21 +125,37 @@ class TestWework:
         """
         r = requests.get(
             f"https://qyapi.weixin.qq.com/cgi-bin/user/delete?access_token={token}&userid={userid}")
-        print(r.json())
+        # print(r.json())
         return r.json()
 
 
-    def test_wework(self,token):
+    @pytest.mark.parametrize("userid, name, mobile", test_create_data())
+    def test_wework(self,token, userid, name, mobile):
         """
         整体测试
         :param token:
         :return:
         """
-        userid = '2020120003'
-        name = '薛之谦'
-        assert "created" == self.test_create(token, userid, '薛大谦', '谦谦', '15200001212')['errmsg']
-        assert "薛大谦" == self.test_get(token, userid)["name"]
-        assert "updated" == self.test_update(token, userid)["errmsg"]
+        # userid = 'MaoQin12'
+        # name = '毛琴'
+        # mobile = '13300001122'
+        # try:
+        #     assert "created" == self.test_create(token, userid, '薛大谦', '谦谦', '13300001122')['errmsg']
+        # except AssertionError as e:
+        #     print("异常信息：", e.__str__())
+        #     if "mobile existed" in e.__str__():
+        #         # 提取以：开头，以'结尾的内容，括号里的是提取的内容，取出第一个内容(提取失败，未解决)
+        #         re_userid = re.findall(":(.*)'$", e.__str__())[0]
+        #         # print(re_userid)
+        #         self.test_delete(token, re_userid)
+        #         assert "created" == self.test_create(token, userid, '薛大谦', '谦谦', '13300001122')['errmsg']
+
+        assert "created" == self.test_create(token, userid, mobile, name, '谦谦')['errmsg']
         assert name == self.test_get(token, userid)["name"]
+        assert "updated" == self.test_update(token, userid)["errmsg"]
+        assert "薛之谦" == self.test_get(token, userid)["name"]
         assert "deleted" == self.test_delete(token,userid)["errmsg"]
         assert 60111 == self.test_get(token, userid)["errcode"]
+
+
+
