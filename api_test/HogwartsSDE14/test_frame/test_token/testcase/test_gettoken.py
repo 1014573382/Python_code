f180de8a3930be75ed90590773567321f6e675b0
guonian <guoxiaoli1512@163.com>
2021-03-27 14:00:31 +0800

    selenium supplement

--- api_test/HogwartsSDE14/test_frame/test_token/testcase/test_gettoken.py
+++ api_test/HogwartsSDE14/test_frame/test_token/testcase/test_gettoken.py
@@ -1,12 +1,13 @@
 #-*- coding:utf-8 -*-
 # @Author : guonian
 # @Time : 2021/01/24 22:42
-from HogwartsSDE14.test_frame.test_token.api.get_token import GetToken
+from HogwartsSDE14.test_frame.test_token.api import get_token
+
 
 class TestToken:
 
     def setup(self):
-        self.gettoken = GetToken()
+        self.gettoken = get_token.GetToken()
 
     def test_gettoken(self):
         assert self.gettoken.get_token().json()['errmsg'] == 'ok'
